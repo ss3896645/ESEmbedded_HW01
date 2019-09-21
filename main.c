d850b0af370d9265b912b8fddcbef1b01505e7a0
jim <jim@jim-VirtualBox.(none)>
2019-09-21 22:18:29 +0800

    [embedded_courses_nclab_stm32] change the switch condition to array of function pointer

--- main.c
+++ main.c
@@ -5,12 +5,14 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
 int main()
 {
 	int a, c;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
+	/*
 	//IF ELSE IF
 	if (b == '+')
 		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
@@ -20,4 +22,8 @@ int main()
 		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
 	else if (b == '/')
 		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+        */
+
+       int (*cmd[])(int a,int c) = {multiply, plus, NULL, minus, NULL, divided};
+       printf("%d\n",cmd[b-42](a,c));
 }
